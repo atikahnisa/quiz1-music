5cc9e5a15f48a29d3e966a10bb1de703b0df0cca
atikahnisa <86197061+atikahnisa@users.noreply.github.com>
2021-07-20 00:01:30 +0700

    Upload Pertama

---
+++ storage/framework/views/55f753d1716e9ee86c7e4945077098803326593e.php
@@ -0,0 +1,24 @@
+
+
+ <?php $__env->startSection('content'); ?>
+
+ <div class="container">
+
+    <h3>Tambah Data Artist</h3>
+    <form action="<?php echo e(url('/artist')); ?>" method="POST">
+        <?php echo csrf_field(); ?>
+        <div class="form-group">
+            <label for="">ID</label>
+            <input type="text" name="artist_id" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="">NAME</label>
+            <input type="text" name="artist_name" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" value="SIMPAN" class="btn btn-primary">
+        </div> 
+    </form>
+ </div>
+ <?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-quis\resources\views/artist/add.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/62fd8a372c8cec61ef16113ee6809e50dc3cf788.php
@@ -0,0 +1,25 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+    <div class="container">
+
+        <h3>Edit Data Artist</h3>
+        <form action="<?php echo e(url('/artist/' . $row->artist_id)); ?>" method="POST">
+            <input name="_method" type="hidden" value="PATCH">
+        <?php echo csrf_field(); ?>
+        <div class="form-group">
+            <label for="">ID</label>
+            <input type="text" name="artist_id" class="form-control" value="<?php echo e($row->artist_id); ?>">
+        </div>
+        <div class="form-group">
+            <label for="">NAME</label>
+            <input type="text" name="artist_name" class="form-control" value="<?php echo e($row->artist_name); ?>">
+        </div>
+        <div class="form-group">
+            <input type="submit" value="UPDATE" class="btn btn-primary">
+        </form>
+     </div>
+
+ <?php $__env->stopSection(); ?> 
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel-quis\resources\views/artist/edit.blade.php ENDPATH**/ ?>
